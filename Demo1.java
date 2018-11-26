54848a1d4d8c115c4420a141524cbec21f75a1bd
ouyangrong <1364782109@qq.com>
2018-11-26 18:43:47 +0800

    update demo1 demo2

--- Demo1.java
+++ Demo1.java
@@ -2,4 +2,5 @@ public static void main(String args[]){
 	int l = 1+2;// e盘修改
 	system.out.println("my is demo1.java");
 	int i = 2*5;// f 盘修改
+	int d = i * l;
 }
--- Demo2.java
+++ Demo2.java
@@ -1,3 +1,4 @@
 public static void main(String args[]) {
 	system.out.println("my is demo2.java");
+	// e update file
 }
