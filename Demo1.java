961cb2528e28c7d1ceb159c29a77e3898534d1eb
ouyangrong <1364782109@qq.com>
2018-11-26 17:31:14 +0800

    初始化

--- Demo1.java
+++ Demo1.java
@@ -1,4 +1,3 @@
-system.out.println("my is demo1.java");
 public static void main(String args[]){
-	
+	system.out.println("my is demo1.java");
 }
--- Demo2.java
+++ Demo2.java
@@ -1,1 +1,3 @@
-system.out.println("my is demo2.java");
+public static void main(String args[]) {
+	system.out.println("my is demo2.java");
+}
